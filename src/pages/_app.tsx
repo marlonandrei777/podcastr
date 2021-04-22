f723b73b808bc9bfe46312816101a15045e441ff
Marlon Andrei <marlon-master@hotmail.com>
2021-04-22 19:34:28 -0300

    player

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,16 +4,40 @@ import { Header } from '../components/Header';
 import { Player } from '../components/Player';
 
 import styles from '../styles/app.module.scss';
+import React, { useState } from 'react';
+import { PlayerContext } from '../contexts/PlayerContext';
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function play(episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function togglePlay() {
+    /* se isPlaying for true vai ser falso, e se for falso vai ser true */
+    setIsPlaying(!isPlaying);
+  }
+
+  /* para o botão play da interface alterar quando usar a função do teclado */
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state)
+  }
+
   return (
-    <div className={styles.warapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState }}>
+      <div className={styles.warapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContext.Provider>
   );
 }
 
